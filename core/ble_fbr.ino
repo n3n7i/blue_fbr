43e57c66b895b7de9bf74c39f04c89c80ea4aa3f
n3n7i <25413904+n3n7i@users.noreply.github.com>
2021-01-28 17:50:20 +0800

    Add files via upload

---
+++ core/ble_fbr.ino
@@ -0,0 +1,15 @@
+#include "core.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  delay(5000);
+  Serial.begin(57600);
+  Serial.println(BLUE_msg);
+}
+
+void loop() {
+
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
