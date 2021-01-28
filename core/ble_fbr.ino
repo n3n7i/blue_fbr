a2c55290b67a50a232909ae530d8d7b26929a65f
n3n7i <25413904+n3n7i@users.noreply.github.com>
2021-01-28 19:24:35 +0800

    Update ble_fbr.ino

--- core/ble_fbr.ino
+++ core/ble_fbr.ino
@@ -1,10 +1,31 @@
 #include "core.h"
 
+
+char xbuffer[1024];
+
 void setup() {
+
+  unsigned long dataAddr;
   // put your setup code here, to run once:
   delay(5000);
   Serial.begin(57600);
   Serial.println(BLUE_msg);
+
+  Serial.println(BLUE_state);
+  Serial.println(BLUE_power);
+
+  Serial.println(BLUE_sfd, HEX);
+
+  dataAddr = BLUE_data;
+  Serial.println(dataAddr, HEX);
+
+  dataAddr = (long) &xbuffer;
+  Serial.println(dataAddr, HEX);
+
+
+  /*if (&NRF_RADIO == &blueRegister)  {Serial.println("OK Same address \n");}  // control   addresses  are the same
+  else {Serial.println("KO Not same address \n");}
+  */
 }
 
 void loop() {
